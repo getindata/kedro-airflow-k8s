5dae447699ecad6617b26a91516c8033e558e052
Michał Żelechowski <michal.zelechowski@getindata.com>
2021-04-27 14:01:03 +0200

    Upgrade dependencies, remove obsolete ContextHelper versions.

--- kedro_airflow_k8s/context_helper.py
+++ kedro_airflow_k8s/context_helper.py
@@ -1,8 +1,6 @@
 from functools import lru_cache
-from pathlib import Path
 
-from kedro import __version__ as kedro_version
-from semver import VersionInfo
+from kedro.framework.session import KedroSession
 
 from .config import PluginConfig
 
@@ -10,7 +8,7 @@ CONFIG_FILE_PATTERN = "airflow-k8s*"
 
 
 class ContextHelper(object):
-    def __init__(self, metadata, env, pipeline_name):
+    def __init__(self, metadata, env, pipeline_name="__default__"):
         self._metadata = metadata
         self._env = env
         self._session = None
@@ -38,8 +36,6 @@ class ContextHelper(object):
 
     @property
     def session(self):
-        from kedro.framework.session import KedroSession
-
         if self._session is None:
             self._session = KedroSession.create(
                 self._metadata.package_name, env=self._env
@@ -57,31 +53,3 @@ class ContextHelper(object):
     @lru_cache()
     def mlflow_config(self):
         return self.context.config_loader.get("mlflow*")
-
-    @staticmethod
-    def init(metadata, env, pipeline_name="__default__"):
-        version = VersionInfo.parse(kedro_version)
-        if version.match(">=0.17.0"):
-            return ContextHelper(metadata, env, pipeline_name)
-        else:
-            return ContextHelper16(metadata, env, pipeline_name)
-
-
-class ContextHelper16(ContextHelper):
-    """ Variant for compatibility with Kedro 1.6 """
-
-    @property
-    def project_name(self):
-        return self.context.project_name
-
-    @property
-    def context(self):
-        from kedro.framework.context import load_context
-
-        return load_context(Path.cwd(), env=self._env)
-
-    @property
-    def session(self):
-        from kedro.framework.session import KedroSession
-
-        return KedroSession.create("", Path.cwd(), env=self._env)
