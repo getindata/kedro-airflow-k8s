3db336d922f47097c69032d9a17d319182feb6d5
Michal Brys <10577534+michalbrys@users.noreply.github.com>
2023-02-28 13:36:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as f:
 # Runtime Requirements.
 INSTALL_REQUIRES = [
     "kedro>=0.16, <0.18",
-    "click<8.0",
+    "click<=8.0",
     "semver~=2.10",
     "python-slugify>=4.0.1",
     "tabulate==0.8.9",
